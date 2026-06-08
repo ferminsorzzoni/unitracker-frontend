344d42a255334c9519b23be66b1fe0a41b2380d0
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 16:07:58 -0300

    cambio al coso de home

--- src/components/header/HeaderHomeButton.tsx
+++ src/components/header/HeaderHomeButton.tsx
@@ -6,8 +6,8 @@ export default function HeaderHomeButton() {
 
     return (
         <Link to={token ? "/dashboard" : "/"} className="flex items-center" title="Home">
-            <img src="/logos/unitracker.svg" alt="" className="w-12 h-12" />
-            <h1 className="text-4xl font-medium text-gray-dark">Unitracker</h1>
+            <img src="/logos/unitracker.svg" alt="" className="w-8 h-8 sm:w-12 sm:h-12" />
+            <h1 className="text-2xl sm:text-4xl font-medium text-gray-dark">Unitracker</h1>
         </Link>
     );
 }
