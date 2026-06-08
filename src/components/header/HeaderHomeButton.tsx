5a59856c4d58d9c2ff1c8a6efd5b2cc97f3accb5
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 16:09:47 -0300

    menos tamaño en home

--- src/components/header/HeaderHomeButton.tsx
+++ src/components/header/HeaderHomeButton.tsx
@@ -6,7 +6,7 @@ export default function HeaderHomeButton() {
 
     return (
         <Link to={token ? "/dashboard" : "/"} className="flex items-center" title="Home">
-            <img src="/logos/unitracker.svg" alt="" className="w-8 h-8 sm:w-12 sm:h-12" />
+            <img src="/logos/unitracker.svg" alt="" className="w-7 h-7 sm:w-12 sm:h-12" />
             <h1 className="text-2xl sm:text-4xl font-medium text-gray-dark">Unitracker</h1>
         </Link>
     );
