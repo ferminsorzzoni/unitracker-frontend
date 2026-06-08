31f992bafa3381961ec0ccf5d5920b7b9afe96a0
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 11:59:50 -0300

    logs

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -4,5 +4,6 @@ import { useAuthStore } from "../stores/authStore";
 export default function PrivateRoute() {
     const token = useAuthStore(state => state.token);
 
+    console.log("Priveateroute", token);
     return token ? <Outlet /> : <Navigate to ="/login" />
 }
