fcc65ce50497ff65793e9a384f47a4d7e296ceb1
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 16:15:48 -0300

    cambio en la card

--- src/components/dashboard/CareerCard.tsx
+++ src/components/dashboard/CareerCard.tsx
@@ -9,9 +9,9 @@ export default function CareerCard({ career }: { career: CareerCardDTO }) {
         <li className="relative">
             <Link to={`/careers/${career.id}`} className="flex flex-col gap-4 rounded-lg border-l-4 p-4 bg-gray-light border-primary shadow-sm hover:shadow-md hover:border-primary-dark transition-all">
                 <div className="flex justify-between">
-                    <h3 className="text-gray-dark font-medium text-2xl">{career.name}</h3>
+                    <h3 className="text-gray-dark font-medium text-lg sm:text-2xl">{career.name}</h3>
                 </div>
-                <p className="text-gray-mid text-xl">{career.institution}</p>
+                <p className="text-gray-mid text-md sm:text-xl">{career.institution}</p>
             </Link>
             <div className="absolute top-4 right-4 z-10">
                 <ShareCareerButton careerId={career.id} />
