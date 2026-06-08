ee2d72da7306e1d179c58a0e7ce762804add067f
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 16:16:58 -0300

    padding right para el titulo de la card

--- src/components/dashboard/CareerCard.tsx
+++ src/components/dashboard/CareerCard.tsx
@@ -8,7 +8,7 @@ export default function CareerCard({ career }: { career: CareerCardDTO }) {
     return (
         <li className="relative">
             <Link to={`/careers/${career.id}`} className="flex flex-col gap-4 rounded-lg border-l-4 p-4 bg-gray-light border-primary shadow-sm hover:shadow-md hover:border-primary-dark transition-all">
-                <div className="flex justify-between">
+                <div className="flex justify-between pr-24">
                     <h3 className="text-gray-dark font-medium text-lg sm:text-2xl">{career.name}</h3>
                 </div>
                 <p className="text-gray-mid text-md sm:text-xl">{career.institution}</p>
