2ae0920aef75177803153586a5a88dbf5edf8bbc
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 10:43:17 -0300

    withcredentials global true

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,10 @@ interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
     _retry?: boolean,
 }
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+const api = axios.create({ 
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+ });
 
 api.interceptors.request.use(config => {
     const token = useAuthStore.getState().token;
